03c1f767ac5a5d6b5a767fd84bf7553d9b9732ff
drZelle <165728760+drZelle@users.noreply.github.com>
2024-11-18 16:35:04 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(props) {
         <footer>
             <div className="bgGradient"></div>
             <div>
-                <h1>APOD PROJECT</h1>
+                <h1>1st Dr.Zelle PROJECT - NASA Picture of the day using API</h1>
                 <h2>{data?.title}</h2>
             </div>
             <button onClick={handleToggleModal}>
